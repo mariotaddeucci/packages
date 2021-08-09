f3a94624f239b23528be093b282419a39cb42528
Mario Taddeucci <mariotaddeucci@gmx.com>
2021-08-08 21:57:33 -0300

    Pre commit (#4)
    * pre commit check
    
    * Added pre-config

--- scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cwd=$(pwd)
-cd "$(dirname "$0")"
-git config advice.ignoredHook false
-[ -e ../.git/hooks/pre-commit ] && rm -- ../.git/hooks/pre-commit
-cp -p _pre-commit ../.git/hooks/pre-commit
-cd ${cwd}
