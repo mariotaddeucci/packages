6ae8b4ece5c6ae20376904323eba07bb9c31d0b4
Mario Taddeucci <mariotaddeucci@gmx.com>
2021-08-09 00:19:20 -0300

    Update pre-commit-staging.py

--- scripts/pre-commit-staging.py
+++ scripts/pre-commit-staging.py
@@ -3,13 +3,12 @@ import shutil
 from sys import argv
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-TMP_DIRNAME = "tmp-pre-commit"
+TMP_DIRNAME = "tmp_pre_commit"
 TMP_URI = os.path.join(BASE_DIR, TMP_DIRNAME)
 
 
 def recursive_overwrite(src, dest, ignore=None):
     if os.path.isdir(src):
-
         if not os.path.isdir(dest):
             os.makedirs(dest)
         files = os.listdir(src)
@@ -21,12 +20,10 @@ def recursive_overwrite(src, dest, ignore=None):
             if f not in ignored:
                 recursive_overwrite(os.path.join(src, f), os.path.join(dest, f), ignore)
     else:
-
         shutil.copyfile(src, dest)
 
 
 if len(argv) > 1:
-
     for file in argv[1:]:
         final_dir = os.path.join(TMP_URI, os.path.dirname(file))
         os.makedirs(final_dir, exist_ok=True)
